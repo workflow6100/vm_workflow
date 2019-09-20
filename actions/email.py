63a91ebd06552f0bb1bc1909a0253896aa3dd2b7
harsh6100 <52690817+harsh6100@users.noreply.github.com>
2019-09-20 12:50:49 +0530

    Update email.py

--- actions/email.py
+++ actions/email.py
@@ -15,9 +15,7 @@ class SendEmail(Action):
       bd = body
       msg.attach(MIMEText(bd, 'plain'))
       server = smtplib.SMTP('smtp.gmail.com', 587)
-      server.ehlo()
       server.starttls()
-      server.ehlo()
       server.login(fromaddr, "harsh6100")
       text = msg.as_string()
       server.sendmail(fromaddr, toaddr, text)
