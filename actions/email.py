45e7ee66a6fd5517aa5c1b347b8237a14e53feae
harsh6100 <52690817+harsh6100@users.noreply.github.com>
2019-09-20 12:44:01 +0530

    Update email.py

--- actions/email.py
+++ actions/email.py
@@ -6,19 +6,18 @@ from st2common.runners.base_action import Action
 
 class SendEmail(Action):
     def run(self,email_to, subject, body):
-      sender_email = "stackstorm.alert@gmail.com"
-      password = "harsh6100"
-      sub = subject
-      to = email_to
-      b1 = body
-        
-      message = MIMEMultipart()
-      message["From"] = sender_email
-      message["To"] = email_to
-      message["Subject"] = subject
-      message["Body"] = b1    
-      
-      context = ssl.create_default_context()
-      with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-          server.login(sender_email, password)
-          server.sendmail(sender_email, email_to, message)
+      fromaddr = "stackstorm.alert@gmail.com"
+      toaddr = email_to
+      msg = MIMEMultipart()
+      msg['From'] = fromaddr
+      msg['To'] = toaddr
+      msg['Subject'] = "Python email"
+      bd = body
+      msg.attach(MIMEText(bd, 'plain'))
+      server = smtplib.SMTP('smtp.gmail.com', 587)
+      server.ehlo()
+      server.starttls()
+      server.ehlo()
+      server.login(fromaddr, "harsh6100")
+      text = msg.as_string()
+      server.sendmail(fromaddr, toaddr, text)
