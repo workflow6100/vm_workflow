89587efb3251232b46ed93c790bc855beda6b274
harsh6100 <52690817+harsh6100@users.noreply.github.com>
2019-09-20 10:28:13 +0530

    Update email.py

--- actions/email.py
+++ actions/email.py
@@ -1,1 +1,24 @@
+import email, smtplib, ssl
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from st2common.runners.base_action import Action
 
+class SendEmail(Action):
+    def run(self,email_to, subject, message):
+      sender_email = "stackstorm.alert@gmail.com"
+      password = "harsh6100"
+      
+      message = MIMEMultipart()
+      message["From"] = sender_email
+      message["To"] = email_to
+      message["Subject"] = subject
+      
+      message.attach(MIMEText(message, "plain"))
+      text = message.as_string()
+      
+      context = ssl.create_default_context()
+      with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+          server.login(sender_email, password)
+          server.sendmail(sender_email, email_to, text)
